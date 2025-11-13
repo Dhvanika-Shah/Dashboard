8d2cf8cb0fced2b01391d4dbdccdb76a22e0357a
Dhvanika-Shah <dhvanika12@gmail.com>
2025-11-13 23:19:21 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+
+# Load Excel file
+df = pd.read_excel("online offline sales.xlsx")
+
+# Calculate Profit
+df["Profit"] = df["Total Selling Value"] - df["Total Buying Value"]
+
+# Profit by Sales Type
+df_saletype = df.groupby("SALE TYPE")["Profit"].sum()
+top_sale_type = df_saletype.idxmax()
+
+st.header("Profit Summary")
+st.metric("Total Profit", f"₹{df['Profit'].sum():,.2f}")
+
+col1, col2 = st.columns(2)
+
+# --- COL 1: Sales Type ---
+with col1:
+    st.markdown("<h3 style='color: green;'>Profit by Sales Type</h3>", unsafe_allow_html=True)
+    st.markdown(f"_{top_sale_type} performed better than offline sales._")
+
+    tab_sales_chart, tab_sales_data = st.tabs(["Charts", "Data"])
+
+    with tab_sales_data:
+        st.write(df_saletype)
+
+    with tab_sales_chart:
+        st.bar_chart(df_saletype)
+
+# --- COL 2: Payment Mode ---
+with col2:
+    df_payment = df.groupby("PAYMENT MODE")["Total Selling Value"].sum()
+
+    st.markdown("<h3 style='color: green;'>Payment Mode Analysis</h3>", unsafe_allow_html=True)
+
+    tab_pay_chart, tab_pay_data = st.tabs(["Charts", "Data"])
+
+    with tab_pay_data:
+        st.write(df_payment)
+
+    with tab_pay_chart:
+        st.bar_chart(df_payment)
